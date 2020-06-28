997291e75260f7e607ad457adfab1d9836d0c290
Fihiz <salome.d.audeteau@gmail.com>
2020-06-28 23:29:05 +0200

    tail added

--- srcs/container_config.sh
+++ srcs/container_config.sh
@@ -1,24 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Here we finish to set our server !"
 
 #NGINX SETUP
+cd ..
 mkdir -p /var/www/localhost
-chown -R $USER:$USER /var/www/localhost
-cp localhost-conf /etc/nginx/sites-available/localhost
+#chown -R $USER:$USER /var/www/localhost
+cp /root/localhost-conf /etc/nginx/sites-available/localhost
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
 
-cd ssl
+#SSL SETUP FOR SECURITY
+cd /root/ssl/
 chmod +x mkcert
 ./mkcert -install
 ./mkcert localhost
-cd ..
+cd ../../
 
 #TEST
-cp info.php /var/www/localhost/info.php
-cp test.html /var/www/localhost/test.html
+cp /root/info.php /var/www/localhost/info.php
+cp /root/test.html /var/www/localhost/test.html
 
 #STARTING
 /etc/init.d/php7.3-fpm start
 service nginx configtest
 service nginx restart
+
+#TO KEEP THE CONTAINER UP
+tail -f /var/log/nginx/access.log /var/log/nginx/error.log
