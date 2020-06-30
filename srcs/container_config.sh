2628d5749616b7d8cebd01204be0e992e8b3d3d3
Fihiz <salome.d.audeteau@gmail.com>
2020-06-30 14:28:33 +0200

    improving files from train

--- srcs/container_config.sh
+++ srcs/container_config.sh
@@ -2,47 +2,43 @@
 
 echo "Here we finish to set our server !"
 
-#NGINX SETUP
+#NGINX SETUP -> ACTIVATING VIRTUAL HOST (BE CAREFUL TO ADD FULL RIGHTS ON FOLDERS AND FILES, www-data is user by default of nginx)
 mkdir -p /var/www/localhost
-chown -R $USER:$USER /var/www/localhost
+#chown -R $USER:$USER /var/www/localhost
+chown -R www-data -R /var/www/localhost
 cp localhost-conf /etc/nginx/sites-available/localhost
 ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/
 
-#SSL SETUP FOR SECURITY
+#SSL SETUP FOR SECURITY BETWEEN SERVER AND NAVIGATOR -> HTTP TO HTTPS
 cd ssl
 chmod +x mkcert
 ./mkcert -install
 ./mkcert localhost
 cd ..
 
-#DATABASE STARTER PACK -> CREATE AN ADMINISTRATIVE MYSQL USER
-service mysql start
-echo "CREATE DATABASE data;" | mysql -u root
-echo "CREATE USER 'user'@'localhost' IDENTIFIED BY 'pass';" | mysql -u root
-#echo "SET password FOR 'tester'@'localhost' = password('pass');" | mysql -u root
-echo "GRANT ALL PRIVILEGES ON data.* TO 'user'@'localhost' IDENTIFIED BY 'pass';" | mysql -u root
+#WORDPRESS SETUP -> COMPRESSED FILES DOWNLOADED FROM OFFICIAL WEBSITE WITH WGET
+cp websrcs/wordpress.tar.gz /var/www/localhost/
+cd /var/www/localhost/
+tar -xzvf wordpress.tar.gz
+rm wordpress.tar.gz
+
+
+#DATABASE AND USER SETUP FOR WORDPRESS -> NEVER DOING IT WITH ADMINISTRATOR SYSTEM FOR SECURITY REASONS
+echo "CREATE DATABASE wordpress;" | mysql -u root 
+echo "CREATE USER 'wordpress'@'localhost';" | mysql -u root
+echo "SET password FOR 'wordpress'@'localhost' = password('mon_password');" | mysql -u root
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost' IDENTIFIED BY 'mon_password';" | mysql -u root
 echo "FLUSH PRIVILEGES;" | mysql -u root
 
-echo "DB and user are created, let's try to fill our table."
+#USER APPLICATION SETUP -> TO LIMIT SCOPE OF ACCESS IN CASE OF INTRUSION
+adduser appuser
+chown appuser:www-data -R /var/www/*
+chmod -R 710 -R /var/www/*
 
-echo "CREATE TABLE data.todo_list (item_id INT AUTO_INCREMENT, content VARCHAR(255), PRIMARY KEY(item_id));" | mysql -u root
-
-echo "INSERT INTO data.todo_list (content) VALUES ('Patate, poisson');" | mysql -u root
-echo "INSERT INTO data.todo_list (content) VALUES ('Manucure ?');" | mysql -u root
-
-echo "SELECT * FROM data.todo_list;" | mysql -u root
-
-
-#TEST
-#cp info.php /var/www/localhost/info.php
-echo "<?php phpinfo(); ?>" > /var/www/localhost/phpinfo.php
-cp todo_tests/test.html /var/www/localhost/test.html
-cp todo_tests/todo_list.php /var/www/localhost/todo_list.php
-
-#STARTING
+#STARTING COMMUNICATION BETWEEN PHP AND NGINX WITH OUR CONFIG
 /etc/init.d/php7.3-fpm start
 service nginx configtest
 service nginx restart
 
-#TO KEEP THE CONTAINER UP
+#TO KEEP THE CONTAINER UP -> INFINITE LOOP SHOWING UP LOGS
 tail -f /var/log/nginx/access.log /var/log/nginx/error.log
